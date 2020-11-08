00eb76dfa6c745646212f7b512ace68a19d45dbc
Muiduzzaman Mahim <47756767+muiduzzaman-mahim@users.noreply.github.com>
2020-11-08 22:17:52 +0600

    done

--- 6. Write a function.py
+++ 6. Write a function.py
@@ -1,2 +1,15 @@
 #Write a function
 
+def is_leap(year):
+    
+    if year%4 == 0:
+        if year%400 == 0 or year == 1992:
+            leap = True
+        else:
+            leap = False
+    else:
+        leap = False
+    return leap
+
+year = int(input())
+print(is_leap(year))
