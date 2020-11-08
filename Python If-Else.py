c69457d61521a315f90d785bc77d8e5328083a17
Muiduzzaman Mahim <47756767+muiduzzaman-mahim@users.noreply.github.com>
2020-11-08 20:02:08 +0600

    done

---
+++ Python If-Else.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+n = int(input())
+
+if n%2 == 1:
+    print('Weird')
+else:
+    if n >= 2 and n <= 5: 
+        print('Not Weird')
+    else:
+        if n >= 6 and n <= 20:
+            print('Weird')
+        else:
+            print('Not Weird')
+    
